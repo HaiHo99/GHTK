f81c69b1f86dd5bbf879405916a53dbd1bc990b2
HaiHo99 <44635640+HaiHo99@users.noreply.github.com>
2021-06-10 12:12:51 +0700

    Update Consumer.java

--- GHTK/src/Week2/Consumer.java
+++ GHTK/src/Week2/Consumer.java
@@ -1,50 +1,57 @@
 package Week2;
 import org.apache.avro.Schema;
-import org.apache.avro.generic.GenericRecord;
-import org.apache.avro.io.Decoder;
-import org.apache.avro.io.DecoderFactory;
-import org.apache.avro.specific.SpecificDatumReader;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.serialization.ByteArrayDeserializer;
 
+import org.apache.avro.generic.GenericData;
+
+import org.apache.avro.generic.GenericDatumWriter;
+import org.apache.avro.generic.GenericRecord;
+
+import org.apache.avro.io.*;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.ByteArraySerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.time.Duration;
-import java.util.Arrays;
 import java.util.Properties;
 
-public class Consumer {
+public class Producer {
     public static void main(String[] args) throws IOException {
-        Schema schema = new Schema.Parser().parse(new File("C:\\Users\\Hai\\Desktop\\haiht34\\emp.avsc"));
+        Schema schema = new Schema.Parser().parse(new File("C:\\Users\\Hai\\eclipse-workspace\\GHTK\\src\\Week2\\emp.avsc"));
+        String topic="haiht34_serializer2";
+        GenericRecord e1=new GenericData.Record(schema);
+        GenericData.Record ad = new GenericData.Record(schema.getField("address").schema());
+        ad.put("pinCode", 10000);
+        ad.put("streetName","Pham Hung");
+        e1.put("ID",34);
+        e1.put("email","ghtk@ghtk");
+        e1.put("name","Hai");
+        e1.put("address",ad);
 
 
-        Properties properties =new Properties();
-        properties.put("bootstrap.servers","10.140.0.13:9092");
-        properties.put("group.id","haiht34_v11");
-        properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class.getName());
-        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        Schema.Parser parser = new Schema.Parser();
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        DatumWriter<GenericRecord> datumWriter = new GenericDatumWriter<GenericRecord>(schema);
+        Encoder binaryEncoder = EncoderFactory.get().binaryEncoder(out, null);
+        datumWriter.write(e1,binaryEncoder);
+        binaryEncoder.flush();
+        out.close();
+        byte[] serializedBytes = out.toByteArray();
+        System.out.println(serializedBytes.toString());
 
 
-        KafkaConsumer<String,byte[]> consumers=new KafkaConsumer<String, byte[]>(properties);
-        consumers.subscribe(Arrays.asList("haiht34_serializer2"));
 
+        Properties properties=new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"10.140.0.13:9092");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ByteArraySerializer.class.getName());
+        KafkaProducer<String, byte[]> producer=new KafkaProducer<String,byte[]>(properties);
 
-        while (true) {
-            ConsumerRecords<String, byte[]> records = consumers.poll(Duration.ofMillis(1000));
-            for (ConsumerRecord<String, byte[]> record : records) {
-                byte[] data=record.value();
-                SpecificDatumReader<GenericRecord> reader = new SpecificDatumReader<GenericRecord>(schema);
-                Decoder decoder = DecoderFactory.get().binaryDecoder(data, null);
-                GenericRecord user = reader.read(null,decoder);
-                System.out.println(user);
-            }
-        }
+        ProducerRecord<String, byte[]> producerRecord= new ProducerRecord<String,byte[]>(topic,null,serializedBytes);
+        producer.send(producerRecord);
+        producer.close();
 
     }
-
 }
